5efd1c9cf1f8afe30f99d0c13ad373971fd22428
mx-smblt <mx.smblt@gmail.com>
2025-10-18 21:14:44 +0300

    Refactored code.

--- model/phone.py
+++ model/phone.py
@@ -10,9 +10,9 @@ from model.field import Field
 class Phone(Field):
     """Class for storing phone numbers. Has format validation (10 digits)"""
 
-    __pattern__: re.Pattern = re.compile(r"^\d{10}$")
+    __pattern: re.Pattern = re.compile(r"^\d{10}$")
 
     def __init__(self, number: str):
-        if re.fullmatch(self.__pattern__, number) is None:
+        if re.fullmatch(Phone.__pattern, number) is None:
             raise InvalidPhoneNumberError(number)
         super().__init__(number)
