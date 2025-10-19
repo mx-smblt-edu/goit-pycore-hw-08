39ab78ba3cc251e3c44742fd282fc6bcc684cc4d
mx-smblt <mx.smblt@gmail.com>
2025-10-19 21:34:25 +0300

    Refactored code.

--- error/invalid_birthday_error.py
+++ error/invalid_birthday_error.py
@@ -19,4 +19,5 @@ class InvalidBirthdayError(Exception):
     """
 
     def __init__(self, date: str):
-        self.message = f"{Fore.RED}[ERROR]{Style.RESET_ALL} Invalid date format '{date}'. Use {Fore.GREEN}DD.MM.YYYY{Style.RESET_ALL}."
+        self.message = (f"{Fore.RED}[ERROR]{Style.RESET_ALL} Invalid date format '{date}'. "
+                        f"Use {Fore.GREEN}DD.MM.YYYY{Style.RESET_ALL}.")
